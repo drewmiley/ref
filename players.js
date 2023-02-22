45a99e2c2814a5de7cf68c05a915b892771ccbeb
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-02-22 00:58:48 +0000

    Corrects values in players

--- players.js
+++ players.js
@@ -1,14 +1,14 @@
 const player = (initial, preferenceBefore) => ({ initial, preferenceBefore })
 
 const players = [
-	player('DD', true),
-	player('DM', true),
+	player('DD', false),
+	player('DM', false),
 	player('JC', true),
-	player('JK', true),
+	player('JK', false),
 	player('ND', true),
 	player('PC', true),
-	player('MS', true),
-	player('SC', true)
+	player('MS', undefined),
+	player('SC', undefined)
 ]
 
 console.log(players)
