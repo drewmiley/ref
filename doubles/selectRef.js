331b8e8cc5768f99511467b8680c377c0bed762e
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-02-22 01:05:39 +0000

    selectRef now has access to players

--- doubles/selectRef.js
+++ doubles/selectRef.js
@@ -1,5 +1,10 @@
 function selectRef({ playedSix, refSix, one, two, three }) {
 	console.log(players);
+	const playedSixPlayer = players.find(player => player.initial == playedSix);
+	const refSixPlayer = players.find(player => player.initial == refSix);
+	const onePlayers = [players.find(player => player.initial == one[0]), players.find(player => player.initial == one[1])];
+	const twoPlayers = [players.find(player => player.initial == two[0]), players.find(player => player.initial == two[1])];
+	const threePlayers = [players.find(player => player.initial == three[0]), players.find(player => player.initial == three[1])];
 	document.getElementById('refOne').innerText = 'ONE';
 	document.getElementById('refTwo').innerText = 'TWO';
 	document.getElementById('refThree').innerText = 'THREE';
--- singles/selectRef.js
+++ singles/selectRef.js
@@ -1,5 +1,11 @@
 function selectRef({ one, two, three, four, five, six }) {
 	console.log(players);
+	const onePlayer = players.find(player => player.initial == one);
+	const twoPlayer = players.find(player => player.initial == two);
+	const threePlayer = players.find(player => player.initial == three);
+	const fourPlayer = players.find(player => player.initial == four);
+	const fivePlayer = players.find(player => player.initial == five);
+	const sixPlayer = players.find(player => player.initial == six);
 	document.getElementById('refOne').innerText = 'ONE';
 	document.getElementById('refTwo').innerText = 'TWO';
 	document.getElementById('refThree').innerText = 'THREE';
