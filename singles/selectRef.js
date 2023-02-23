a69af1c0d2ec8a0959b8d05e52af98629f960a87
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-02-23 00:18:10 +0000

    Fixes some bugs with singles

--- singles/selectRef.js
+++ singles/selectRef.js
@@ -4,45 +4,42 @@ const getInitialAllocation = initialArray => {
 	if (initialArray.length == 2) return Math.random < 0.5 ? initialArray[0] : initialArray[1];
 }
 
-const getInitialsNotAllocatedJoinedUnlessPlayingFrame = (notAllocatedInitials, playingInitials) => {
-	return notAllocatedInitials.filter(initial => initial !== playingInitials).join('/');
-}
+const getInitialsNotAllocatedJoinedUnlessPlayingFrame = (notAllocatedInitials, cannotRefInitials) =>
+	notAllocatedInitials.filter(initial => !cannotRefInitials.includes(initial)).join('/');
 
 function selectRef({ one, two, three, four, five, six }) {
-	const onePlayer = players.find(player => player.initial == one);
-	const twoPlayer = players.find(player => player.initial == two);
-	const threePlayer = players.find(player => player.initial == three);
-	const fourPlayer = players.find(player => player.initial == four);
-	const fivePlayer = players.find(player => player.initial == five);
-	const sixPlayer = players.find(player => player.initial == six);
-
-	const oneOptions = [...twoPlayer.preferenceBefore ? [twoPlayer.initial] : []];
-	const twoOptions = [...!onePlayer.preferenceBefore ? [onePlayer.initial] : [], ...threePlayer.preferenceBefore ? [threePlayer.initial] : []];
-	const threeOptions = [...!twoPlayer.preferenceBefore ? [twoPlayer.initial] : [], ...fourPlayer.preferenceBefore ? [fourPlayer.initial] : []];
-	const fourOptions = [...!threePlayer.preferenceBefore ? [threePlayer.initial] : [], ...fivePlayer.preferenceBefore ? [fivePlayer.initial] : []];
-	const fiveOptions = [...!fourPlayer.preferenceBefore ? [fourPlayer.initial] : [], ...sixPlayer.preferenceBefore ? [sixPlayer.initial] : []];
-	const sixOptions = [...!fivePlayer.preferenceBefore ? [fivePlayer.initial] : []];
-
-	const playerInitials = [one, two, three, four, five, six];
-	const initialAllocation = [
-		getInitialAllocation(oneOptions),
-		getInitialAllocation(twoOptions),
-		getInitialAllocation(threeOptions),
-		getInitialAllocation(fourOptions),
-		getInitialAllocation(fiveOptions),
-		getInitialAllocation(sixOptions)
+	const playing = [
+		players.find(player => player.initial == one),
+		players.find(player => player.initial == two),
+		players.find(player => player.initial == three),
+		players.find(player => player.initial == four),
+		players.find(player => player.initial == five),
+		players.find(player => player.initial == six)
 	];
 
-	const initialsNotAllocatedFrame = playerInitials.filter(initial => !initialAllocation.includes(initial));
-
-	const fullAllocation = [
-		initialAllocation[0] || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, onePlayer.initial),
-		initialAllocation[1] || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, twoPlayer.initial),
-		initialAllocation[2] || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, threePlayer.initial),
-		initialAllocation[3] || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, fourPlayer.initial),
-		initialAllocation[4] || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, fivePlayer.initial),
-		initialAllocation[5] || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, sixPlayer.initial)
+	const wantsToRefOptions = [
+		[...playing[1].preferenceBefore ? [playing[1].initial] : []],
+		[...!playing[0].preferenceBefore ? [playing[0].initial] : [], ...playing[2].preferenceBefore ? [playing[2].initial] : []],
+		[...!playing[1].preferenceBefore ? [playing[1].initial] : [], ...playing[3].preferenceBefore ? [playing[3].initial] : []],
+		[...!playing[2].preferenceBefore ? [playing[2].initial] : [], ...playing[4].preferenceBefore ? [playing[4].initial] : []],
+		[...!playing[3].preferenceBefore ? [playing[3].initial] : [], ...playing[5].preferenceBefore ? [playing[5].initial] : []],
+		[...!playing[4].preferenceBefore ? [playing[4].initial] : []]
 	];
+
+	const cannotRefOptions = [
+		[playing[0].initial, ...!playing[1].preferenceBefore ? [playing[1].initial] : []],
+		[playing[1].initial, ...playing[0].preferenceBefore ? [playing[0].initial] : [], ...!playing[2].preferenceBefore ? [playing[2].initial] : []],
+		[playing[2].initial, ...playing[1].preferenceBefore ? [playing[1].initial] : [], ...!playing[3].preferenceBefore ? [playing[3].initial] : []],
+		[playing[3].initial, ...playing[2].preferenceBefore ? [playing[2].initial] : [], ...!playing[4].preferenceBefore ? [playing[4].initial] : []],
+		[playing[4].initial, ...playing[3].preferenceBefore ? [playing[3].initial] : [], ...!playing[5].preferenceBefore ? [playing[5].initial] : []],
+		[playing[5].initial, ...playing[4].preferenceBefore ? [playing[4].initial] : []]
+	]
+
+	const initialAllocation = wantsToRefOptions.map(getInitialAllocation);
+
+	const initialsNotAllocatedFrame = playing.map(player => player.initial).filter(initial => !initialAllocation.includes(initial));
+
+	const fullAllocation = initialAllocation.map((initial, index) => initial || getInitialsNotAllocatedJoinedUnlessPlayingFrame(initialsNotAllocatedFrame, cannotRefOptions[index]));
 
 	document.getElementById('refOne').innerText = fullAllocation[0];
 	document.getElementById('refTwo').innerText = fullAllocation[1];
